4cf158ae8ab69785fefa1fb2361b6afdc91d1251
matt041399 <mattnovo@udel.edu>
2020-12-02 14:10:19 -0500

    pushing stuff

--- Angular/ExampleUI/src/app/pages/home/home.component.ts
+++ Angular/ExampleUI/src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProjectsService } from 'src/app/services/projects.service';
 
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -8,13 +9,16 @@ import { ProjectsService } from 'src/app/services/projects.service';
 })
 export class HomeComponent implements OnInit {
   movies=[];
+  query;
   constructor(private projSvc:ProjectsService) { 
-    projSvc.getProjects().subscribe(result=>{
+    
+    this.query = "Spongebob"
+    projSvc.getProjects(this.query).subscribe(result=>{
       let length = result.results.length;
       for(let i = 0; i < length; i++){
         this.movies.push(result.results[i].title);
       }
-      console.log(result);
+      
       //this.semesters = [1];
     })
   }
--- Angular/ExampleUI/src/app/services/projects.service.ts
+++ Angular/ExampleUI/src/app/services/projects.service.ts
@@ -6,11 +6,11 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class ProjectsService {
-
-  private path="https://api.themoviedb.org/3/search/movie?api_key=19c64f4f9f25a75eb15fb7d09a07f1a4&query=spongebob"
+  private q = "Jaws";
+  private path="https://api.themoviedb.org/3/search/movie?api_key=19c64f4f9f25a75eb15fb7d09a07f1a4&query=";
   constructor(private http:HttpClient) { }
 
-  getProjects(): Observable<any>{
-    return this.http.get(this.path);
+  getProjects(query: any): Observable<any>{
+    return this.http.get(this.path + query);
   }
 }
