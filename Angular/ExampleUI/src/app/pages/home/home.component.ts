52c148beb59bbcc0f5f294812f340872bb6aea22
matt041399 <mattnovo@udel.edu>
2020-12-04 18:53:03 -0500

    "partial" working

--- Angular/ExampleUI/src/app/pages/home/home.component.ts
+++ Angular/ExampleUI/src/app/pages/home/home.component.ts
@@ -15,14 +15,21 @@ export class HomeComponent implements OnInit {
     search: new FormControl('')
   });
   movies = [];
+  posters = [];
+  urlStart = "https://image.tmdb.org/t/p/w500/";
   query = "a";
   constructor(private projSvc: ProjectsService) {
+    console.log(this.posters);
+    //this.posters.push("https://image.tmdb.org/t/p/w500/i5xiwdSsrecBvO7mIfAJixeEDSg.jpg");
+    //this.posters.push("https://image.tmdb.org/t/p/w500/i5xiwdSsrecBvO7mIfAJixeEDSg.jpg");
+
     //this.query = this.myForm.value;
     //console.log(this.myForm.value);
     projSvc.getProjects(this.query).subscribe(result => {
       let length = result.results.length;
       for (let i = 0; i < length; i++) {
         this.movies.push(result.results[i].title);
+        this.posters.push(this.urlStart + result.results[i].poster_path);
       }
     })
 
@@ -34,10 +41,13 @@ export class HomeComponent implements OnInit {
   onSubmit(data) {
     this.query = data.search;
     this.movies = [];
+    this.posters = [];
     this.projSvc.getProjects(this.query).subscribe(result => {
       let length = result.results.length;
       for (let i = 0; i < length; i++) {
         this.movies.push(result.results[i].title);
+        //console.log(this.urlStart + result.results[i].poster_path)
+        this.posters.push(this.urlStart + result.results[i].poster_path);
       }
     })
     //console.log(this.movies);
